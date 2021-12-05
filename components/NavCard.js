fdf193accb303ff88d466002f73f0d0fcf0ca996
Simeng He <simenghe@Simengs-MBP.cgocable.net>
2021-12-05 16:54:07 -0500

    M1

---
+++ components/NavCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavCard({ title, description, link }) {
+    return (
+        <a
+            href={link}
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+        >
+            <h3 className="text-2xl font-bold">{title} &rarr;</h3>
+            <p className="mt-4 text-xl">
+                {description}
+            </p>
+        </a>
+    )
+}
+
+export default NavCard
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,36 @@
 import Head from 'next/head'
+import Router from 'next/router'
+import NavBar from '../components/NavCard.js'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Simeng He Personal Site.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
+          Hi, I am Simeng!{' '}
+        </h1>
+        <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-red-600" href="https://nextjs.org">
-            Next.js!
+            my website.
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
+          University of Waterloo{' '}
+          <code className="p-1 font-mono text-lg bg-gray-100 rounded-md">
+            Computer Science
           </code>
+          {' '}senior
         </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+          <NavBar title="Github" description="Find some of my projects" link="https://github.com/simenghe" />
 
           <a
             href="https://nextjs.org/learn"
@@ -74,7 +72,8 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <code className="px-2">React + TailwindCSS + Next.js</code>
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" /> */}
         </a>
       </footer>
     </div>
